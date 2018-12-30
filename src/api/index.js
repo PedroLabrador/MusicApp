0ca6ff84526f6e0286e10598b47cc6fa5c9275f6
PedroLabrador <pedrolabrador3@outlook.com>
2018-12-30 15:36:22 -0600

    Fixed app

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import config from "./config"
 const { api_key } = config
 
 export default function getArtists(country) {
-	const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${api_key}&format=json`
+	const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${api_key}&format=json`
 	return fetch(URL)
 		.then(res => res.json())
 		.then(json => json.topartists.artist)
